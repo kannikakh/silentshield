d6225facf691585556d6e6148e846e3503810c42
Sweetha-s <sweetha@student.tce.edu>
2026-01-16 13:21:10 +0530

    commit

--- backend/app/firebase_config.py
+++ backend/app/firebase_config.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-FIREBASE_KEY_PATH = "../serviceAccountKey.json"
+FIREBASE_KEY_PATH = "D:/Web Page/Portfolio/silentshield/backend/serviceAccountKey.json"
 
 cred = credentials.Certificate(FIREBASE_KEY_PATH)
 
